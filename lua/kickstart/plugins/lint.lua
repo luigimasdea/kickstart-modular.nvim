3c55c7be9066011dca29ea23086668d34cb79b8c
Luigi Masdea <luigimasdea0@gmail.com>
2026-06-17 15:59:54 +0200

    feat: add vale

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -15,18 +15,18 @@ return {
     --
     -- However, note that this will enable a set of default linters,
     -- which will cause errors unless these tools are available:
-    -- {
+    lint.linters_by_ft = {
     --   clojure = { "clj-kondo" },
     --   dockerfile = { "hadolint" },
     --   inko = { "inko" },
     --   janet = { "janet" },
     --   json = { "jsonlint" },
-    --   markdown = { "vale" },
-    --   rst = { "vale" },
+      markdown = { "vale" },
+      rst = { "vale" },
     --   ruby = { "ruby" },
     --   terraform = { "tflint" },
-    --   text = { "vale" }
-    -- }
+      text = { "vale" }
+    }
     --
     -- You can disable the default linters by setting their filetypes to nil:
     -- lint.linters_by_ft['clojure'] = nil
