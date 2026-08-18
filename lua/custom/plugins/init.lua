6797198bf330d5a1588d5e04d6fd1231bd4888ae
Luigi Masdea <luigimasdea0@gmail.com>
2026-08-18 17:21:02 +0200

    feat: add typst_preview and spider plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,7 +13,38 @@ return {
     config = function() require('bufferline').setup {} end,
   },
 
+  -- {
+  --   'kaarmu/typst.vim',
+  -- },
+
   {
-    'kaarmu/typst.vim',
+    'chomosuke/typst-preview.nvim',
+    -- Carica il plugin solo quando apri un file Typst
+    ft = 'typst',
+    version = '1.*',
+    -- Scarica e aggiorna le dipendenze esterne necessarie per la preview
+    build = function() require('typst-preview').update() end,
+    opts = {
+      -- Di default apre il browser predefinito del sistema.
+      -- Se vuoi forzarne uno specifico (es. Firefox), decommenta e modifica la riga sotto:
+      -- open_cmd = "firefox %s -P default --class typst-preview",
+    },
+    keys = {
+      -- Premi Spazio + t + p per aprire/chiudere l'anteprima
+      { '<leader>tp', '<cmd>TypstPreviewToggle<cr>', desc = 'Toggle Typst Preview' },
+      -- Premi Spazio + t + s per forzare la sincronizzazione (se dovesse servire)
+      { '<leader>ts', '<cmd>TypstPreviewSync<cr>', desc = 'Sync Typst Preview' },
+    },
+  },
+
+  {
+    'chrisgrieser/nvim-spider',
+    lazy = true,
+    keys = {
+      { 'w', "<cmd>lua require('spider').motion('w')<cr>", mode = { 'n', 'o', 'x' }, desc = 'Spider-w' },
+      { 'e', "<cmd>lua require('spider').motion('e')<cr>", mode = { 'n', 'o', 'x' }, desc = 'Spider-e' },
+      { 'b', "<cmd>lua require('spider').motion('b')<cr>", mode = { 'n', 'o', 'x' }, desc = 'Spider-b' },
+      { 'ge', "<cmd>lua require('spider').motion('ge')<cr>", mode = { 'n', 'o', 'x' }, desc = 'Spider-ge' },
+    },
   },
 }
